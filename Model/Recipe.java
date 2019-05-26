584113d25dbc068cceceb13d466a7f5e7a836b13
BlaineOb <blnob567@gmail.com>
2019-05-26 13:33:40 -0400

    Added Recipe Object

---
+++ Model/Recipe.java
@@ -0,0 +1,16 @@
+package Model;
+
+public class Recipe {
+
+    private String name;
+    private String date;
+
+    public Recipe(String name, String date) {
+        this.name = name;
+        this.date = date;
+    }
+
+    public String getName() { return name; }
+
+    public String getDate() { return date; }
+}
