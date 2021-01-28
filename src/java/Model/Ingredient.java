5b1f26481e94fb7ed5da55788f5f288c523d2fb4
BlaineOb <blnob567@gmail.com>
2021-01-27 20:36:04 -0500

    Standardized project structure

---
+++ src/java/Model/Ingredient.java
@@ -0,0 +1,32 @@
+package java.Model;
+
+public class Ingredient {
+
+    private String recipeId;
+    private String name;
+    private String amount;
+    private String type;
+
+    public Ingredient(String recipeId, String name, String amount, String type) {
+        this.recipeId = recipeId;
+        this.name = name;
+        this.amount = amount;
+        this.type = type;
+    }
+
+    public String getRecipeId() {
+        return recipeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
