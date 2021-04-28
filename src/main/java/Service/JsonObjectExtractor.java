c052008c66c861de3d123291245dbd4184ace004
BlaineOb <blnob567@gmail.com>
2021-04-28 15:46:33 -0400

    Service refactoring

---
+++ src/main/java/Service/JsonObjectExtractor.java
@@ -0,0 +1,28 @@
+package Service;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonObjectExtractor {
+    private final JsonObjectBuilder builder = Json.createBuilderFactory(null).createObjectBuilder();
+
+    public String extractJsonString(ResultSet rs, String[] columnNames, String[] propertyNames) throws SQLException {
+        List<JsonObject> jsonList = new ArrayList<>();
+        while(rs.next())
+            buildJSONObject(rs, columnNames, propertyNames, jsonList);
+        rs.close();
+        return jsonList.toString();
+    }
+
+    private void buildJSONObject(ResultSet rs, String[] columnNames, String[] propertyNames, List<JsonObject> jsonList) throws SQLException {
+        for (int i = 0; i < columnNames.length; i++)
+            builder.add(propertyNames[i], rs.getString(columnNames[i]));
+        JsonObject jsonObject = builder.build();
+        jsonList.add(jsonObject);
+    }
+}
