5deda7490beeb2a0c620156f26267df9f9f66aca
BlaineOb <blnob567@gmail.com>
2021-04-28 15:35:01 -0400

    Delete Constants.java

--- Constants.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-public class Constants {
-    public static final String JDBC_DRIVER = "org.postgresql.Driver";
-    public static final String DB_URL = "jdbc:postgresql://localhost:5433/BrewTool";
-    public static final String DB_USER = "dod";
-    public static final String DB_PASSWORD = "1";
-}
